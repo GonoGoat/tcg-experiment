7ca89ebc5cf8320125d0546456d355b4d6889fc6
GonoGoat <gauth.vrsgn@outlook.be>
2025-05-25 22:46:29 +0200

    refactor: Update deck reducers + Comments

--- src/context/DeckStore/reducers.ts
+++ src/context/DeckStore/reducers.ts
@@ -1,25 +1,22 @@
 import { DeckState } from "types/context.types"
 import { genericCard } from "types/ygopro.types"
-import { CARD_ZONES } from "types/global.enum"
+import { CARD_ZONES, ED_MONSTER_TYPES } from "types/global.enum"
 
 /**
- * TODO replace with REGEX
  * @param {string} type Card type
  * @returns true/false if this card type belongs to the extra deck
  */
 const belongsToExtraDeck = (type: string) => {
-    return(        
-        type === 'XYZ Monster' ||
-        type === 'Pendulum Effect Fusion Monster' ||
-        type === 'Synchro Monster' ||
-        type === 'Synchro Pendulum Effect Monster' ||
-        type === 'Synchro Tuner Monster' ||
-        type === 'XYZ Pendulum Effect Monster' ||
-        type === 'Fusion Monster' ||
-        type === 'Link Monster'
-    )
+    return(new RegExp(Object.values(ED_MONSTER_TYPES).map(type => `(${type})`).join('|')).test(type.toLowerCase()))
 }
 
+/**
+ * Add a card to a specific destination
+ * @param state Deck state
+ * @param payload Card to be added
+ * @param dest Where to send the card
+ * @returns State with a new card in the destination collection
+ */
 export function addCard (state: DeckState, payload: genericCard, dest: CARD_ZONES) {
     return {
         ...state,
@@ -27,6 +24,12 @@ export function addCard (state: DeckState, payload: genericCard, dest: CARD_ZONE
     }
 }
 
+/**
+ * Dispatch a card between the main deck and the extra deck based on the card type
+ * @param state Deck state
+ * @param payload Card to be added
+ * @returns State with a new card in either main deck or extra deck
+ */
 export function dispatchCard (state: DeckState, payload: genericCard) {
     if (belongsToExtraDeck(payload.type)){
         return {
@@ -61,8 +64,6 @@ export function eraseDeck (state: DeckState) {
     return {
         ...state,
         main: [],
-        extra: [],
-        side: [],
-        bulk: []
+        extra: []
     }
 }
--- src/types/global.enum.ts
+++ src/types/global.enum.ts
@@ -2,3 +2,10 @@ export enum CARD_ZONES {
     MAIN = "main",
     EXTRA = "extra",
 }
+
+export enum ED_MONSTER_TYPES {
+    FUSION = "fusion",
+    SYNCHRO = "synchro",
+    XYZ = "xyz",
+    LINK = "link"
+}
