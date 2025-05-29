2d7b7694dd2a79958333578b821cf4a77f69b55d
GonoGoat <gauth.vrsgn@outlook.be>
2025-05-30 00:30:51 +0200

    feat: Create Marking menu

--- src/pages/Statistics.tsx
+++ src/pages/Statistics.tsx
@@ -1,25 +1,45 @@
 import React from 'react'
 
 import 'assets/style/pages/Statistics.css'
-
+import { MARKERS } from 'types/global.enum'
 import useDeckStore from "context/DeckStore/store"
 //import useStatStore from 'context/StatStore/store'
 
 const Statistics =  () => {
 
+    const [activeMarker, setActiveMarker] = React.useState<string>('')
+
     const main = useDeckStore((state) => state.main)
     const extra = useDeckStore((state) => state.extra)
     const side = useDeckStore((state) => state.side)
 
+    function handleMarking (marker: MARKERS) {
+        setActiveMarker(activeMarker===marker?"":marker)
+    }
+
     return (
         <div className="statistics">
-            <div>
-                <h3>Statistics</h3>
+            <div className='markers'>
+                {Object.values(MARKERS).map( (value, index) =>
+                    <div
+                        className={value === activeMarker ?"selected":""}
+                        key={index} 
+                        onMouseDown={() => handleMarking(value)}
+                    >
+                        {value}
+                    </div>
+                )}
             </div>
-            <div>
-                <span><strong>Main size:</strong> {main.length}</span><br/>
-                <span><strong>Extra size:</strong> {extra.length}</span><br/>
-                <span><strong>Side size:</strong> {side.length}</span><br/>
+            <hr/>
+            <div className='results'>
+                <div>
+                    <h3>Statistics</h3>
+                </div>
+                <div>
+                    <span><strong>Main size:</strong> {main.length}</span><br/>
+                    <span><strong>Extra size:</strong> {extra.length}</span><br/>
+                    <span><strong>Side size:</strong> {side.length}</span><br/>
+                </div>
             </div>
         </div>
     )
--- src/types/global.enum.ts
+++ src/types/global.enum.ts
@@ -10,3 +10,14 @@ export enum ED_MONSTER_TYPES {
     XYZ = "xyz",
     LINK = "link"
 }
+
+export enum MARKERS {
+    // Engine : Starter - Extender - (Brick)
+    // Non-Engine : Board Breaker - Hand Trap - (Defensive) - Consistency/Power card
+    ENGINE = "Engine",
+    NON_ENGINE = "Non-Engine",
+    STARTER = "Starter",
+    EXTENDER = "Extender",
+    HAND_TRAP = "Hand Trap",
+    BOARD_BREAKER = "Board Breaker"    
+}
