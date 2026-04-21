92c104b00833f4bcc2eb190b5775ce5da5edfa06
GonoGoat <gauth.vrsgn@outlook.be>
2026-04-21 08:10:47 +0200

    feat: Remove span from card description

--- src/components/Cards/CardDescription.tsx
+++ src/components/Cards/CardDescription.tsx
@@ -16,13 +16,13 @@ const CardDescription: FC<CardDescriptionProps> = (props) => {
     return (
         <div className="card-description">
             {props.cardInfo.name?<h6>{props.cardInfo.name}<br/></h6>:<></>}
-            {props.cardInfo.level?<span>LV: {props.cardInfo.level}<br/></span>:<></>}
-            {props.cardInfo.type?<span>Card Type: {props.cardInfo.type}<br/></span>:<></>}
-            {props.cardInfo.race?<span>Card subtype: {props.cardInfo.race}<br/></span>:<></>}
-            {props.cardInfo.attribute?<span>Attribute: {props.cardInfo.attribute}<br/></span>:<></>}
-            {props.cardInfo.atk?<span>ATK: {props.cardInfo.atk}<br/></span>:<></>}
-            {props.cardInfo.def?<span>DEF: {props.cardInfo.def}<br/></span>:<></>}
-            {props.cardInfo.desc?<span>Description: {props.cardInfo.desc}<br/></span>:<></>} 
+            {props.cardInfo.level?<>LV: {props.cardInfo.level}<br/></>:<></>}
+            {props.cardInfo.type?<>Card Type: {props.cardInfo.type}<br/></>:<></>}
+            {props.cardInfo.race?<>Card subtype: {props.cardInfo.race}<br/></>:<></>}
+            {props.cardInfo.attribute?<>Attribute: {props.cardInfo.attribute}<br/></>:<></>}
+            {props.cardInfo.atk?<>ATK: {props.cardInfo.atk}<br/></>:<></>}
+            {props.cardInfo.def?<>DEF: {props.cardInfo.def}<br/></>:<></>}
+            {props.cardInfo.desc?<>Description: {props.cardInfo.desc}<br/></>:<></>} 
         </div>
     )
     
